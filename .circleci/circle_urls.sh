08fba2b23ed150c6a925e7b922ff35c58c79ed58
Ian Cosden <cosden@users.noreply.github.com>
2020-10-23 14:26:17 -0400

    Update circle_urls.sh

--- .circleci/circle_urls.sh
+++ .circleci/circle_urls.sh
@@ -1,6 +1,10 @@
 REPO_ID=$(curl https://api.github.com/repos/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME} | jq --raw-output '.id')
 echo "Repo ID is ${REPO_ID}"
-BASEURL=https://${CIRCLE_BUILD_NUM}-${REPO_ID}-gh.circle-artifacts.com/0/intersect-training.github.io
-IMGURL=${BASEURL}/images/
+URL=https://${CIRCLE_BUILD_NUM}-${REPO_ID}-gh.circle-artifacts.com/0
+BASEURL=/intersect-training.github.io
+IMGURL=${URL}${BASEURL}/images/
+#BASEURL=https://${CIRCLE_BUILD_NUM}-${REPO_ID}-gh.circle-artifacts.com/0/intersect-training.github.io
+#IMGURL=${BASEURL}/images/
 sed -i "35 s,.*,baseurl: $BASEURL,g" "_config.yml"
 sed -i "48 s,.*,urlimg: $IMGURL,g" "_config.yml"
+sed -i "34 s,,*,url: $URL,g" "_config.yml"
