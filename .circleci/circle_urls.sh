cd434d11c04eb1fb07aa9aa21c5a72fb787b9ac6
Ian Cosden <cosden@users.noreply.github.com>
2022-08-19 12:38:56 -0400

    Update circle_urls.sh

--- .circleci/circle_urls.sh
+++ .circleci/circle_urls.sh
@@ -5,6 +5,7 @@ BASEURL=/intersect-training.github.io
 IMGURL=${URL}${BASEURL}/images/
 #BASEURL=https://${CIRCLE_BUILD_NUM}-${REPO_ID}-gh.circle-artifacts.com/0/intersect-training.github.io
 #IMGURL=${BASEURL}/images/
+echo "URL is ${URL}"
 sed -i "35 s,.*,baseurl: $BASEURL,g" "_config.yml"
 sed -i "48 s,.*,urlimg: $IMGURL,g" "_config.yml"
 sed -i "34 s,.*,url: $URL,g" "_config.yml"
