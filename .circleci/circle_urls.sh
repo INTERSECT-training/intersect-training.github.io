c95eecbed0bee1714934c1d3d42dfd0e5bf1318a
Ian Cosden <cosden@users.noreply.github.com>
2025-09-18 13:51:31 -0400

    Fix CircleCI Preview (#93)
    * Update circle_urls.sh
    
    * Clean up and remove old code

--- .circleci/circle_urls.sh
+++ .circleci/circle_urls.sh
@@ -1,12 +1,3 @@
-REPO_ID=$(curl https://api.github.com/repos/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME} | jq --raw-output '.id')
-echo "Repo ID is ${REPO_ID}"
-
-URL=https://output.circle-artifacts.com/output/job/${CIRCLE_WORKFLOW_JOB_ID}/artifacts/${CIRCLE_NODE_INDEX}
-BASEURL=/intersect-training.github.io
-IMGURL=${URL}${BASEURL}/images/
-
-echo "URL is ${URL}"
-
-sed -i "35 s,.*,baseurl: $BASEURL,g" "_config.yml"
-sed -i "48 s,.*,urlimg: $IMGURL,g" "_config.yml"
-sed -i "34 s,.*,url: $URL,g" "_config.yml"
+BASEURL=https://output.circle-artifacts.com/output/job/${CIRCLE_WORKFLOW_JOB_ID}/artifacts/${CIRCLE_NODE_INDEX}/intersect-training.github.io
+sed -i "8 s,.*,baseurl: $BASEURL,g" "_config.yml"
+sed -i "7 s,.*,url: \"\",g" "_config.yml"
